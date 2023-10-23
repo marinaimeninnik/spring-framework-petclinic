e0e3808b64aac79590bb75190cc40e6b9dcfce06
Maryna Nazarenko <maryna_nazarenko1@epam.com>
2023-10-23 21:25:34 +0200

    working eround bucket name error

--- terraform/dev/backend.tf
+++ terraform/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "arn:aws:s3:eu-central-1:519240998635:accesspoint/acc1"
+    bucket = "jenkinsterraform-state"
     key    = "dev/terraform.tfstate"
     region = "eu-central-1"
   }
