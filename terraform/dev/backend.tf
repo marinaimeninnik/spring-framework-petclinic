af218d44d148bde7dc43c91ddacf7cad7ed8ff34
Maryna Nazarenko <maryna_nazarenko1@epam.com>
2023-10-23 22:38:27 +0200

    another approach to terraform

--- terraform/dev/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "jenkinsterraform-state"
-    key    = "dev/terraform.tfstate"
-    region = "eu-central-1"
-    access_key = "${env.AWS_ACCESS_KEY_ID}"
-    secret_key = "${env.AWS_SECRET_ACCESS_KEY}"
-  }
-}
