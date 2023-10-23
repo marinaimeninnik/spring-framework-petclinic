9ba16d4ad43b56abc7e6ee11cfc4deb6c6abc38d
Maryna Nazarenko <maryna_nazarenko1@epam.com>
2023-10-23 21:57:02 +0200

    working around terraform stage

--- terraform/dev/backend.tf
+++ terraform/dev/backend.tf
@@ -3,5 +3,7 @@ terraform {
     bucket = "jenkinsterraform-state"
     key    = "dev/terraform.tfstate"
     region = "eu-central-1"
+    access_key = "${env.AWS_ACCESS_KEY_ID}"
+    secret_key = "${env.AWS_SECRET_ACCESS_KEY}"
   }
 }
--- terraform/dev/backend.tfvars
+++
@@ -1,5 +0,0 @@
-access_key = "${env.AWS_ACCESS_KEY_ID}"
-secret_key = "${env.AWS_SECRET_ACCESS_KEY}"
-bucket = "jenkinsterraform-state"
-key = "dev/terraform.tfstate"
-region = "eu-central-1" # Change to your AWS region
