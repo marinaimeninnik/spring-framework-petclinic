7f2d17db54c8995ffa78d2ef3c74e7ebde2b2183
Maryna Nazarenko <maryna_nazarenko1@epam.com>
2023-10-23 21:42:32 +0200

    working around aws credentials

---
+++ terraform/dev/backend.tfvars
@@ -0,0 +1,5 @@
+access_key = "YOUR_AWS_ACCESS_KEY"
+secret_key = "YOUR_AWS_SECRET_KEY"
+bucket = "jenkinsterraform-state"
+key = "dev/terraform.tfstate"
+region = "eu-central-1" # Change to your AWS region
--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -24,7 +24,7 @@ variable "instance_count" {
 
 # Example AWS provider configuration
 provider "aws" {
-  region = "eu-central-1a"
+  region = "eu-central-1"
   access_key = var.access_key
   secret_key = var.secret_key
 }
